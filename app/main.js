f61f5caeae9d57ef6c9eff0713b3b81b55fe2bb5
Joabe <joabe.de.silva@gmail.com>
2023-07-21 13:34:06 -0300

    added js lines to index & created async function in main

--- app/main.js
+++ app/main.js
@@ -1,9 +1,11 @@
 let livros = []
-const endPointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+const endPointdaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosdaAPI() 
 
 async function getBuscarLivrosdaAPI() {
-    const res = await fetch(endPointAPI)
+    const res = await fetch(endPointdaAPI)
     livros = await res.json()
-    console.table(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto) //função para mostrar os descontos 
+    // e tmb para manipular com os métodos
 }

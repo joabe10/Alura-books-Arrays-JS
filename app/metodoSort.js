73db580a916e72f99210d987865206038a1e1086
Joabe <joabe.de.silva@gmail.com>
2023-07-21 16:26:39 -0300

    organized the books with .sort method

---
+++ app/metodoSort.js
@@ -0,0 +1,7 @@
+const ordenacao = document.querySelector('#btnOrdenarPorPreco')
+ordenacao.addEventListener('click', ordenarLivrosPorPreco)
+
+function ordenarLivrosPorPreco(){
+    let livrosOrdenados = livros.sort((a,b) => a.preco - b.preco)
+    exibirOsLivrosNaTela(livrosOrdenados)
+}
